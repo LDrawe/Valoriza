b435986fe1375a010dd25021ad1655136fbfd26b
LDrawe <eduardomportilho@hotmail.com>
2022-02-15 00:35:17 -0300

    Aula 2 - Configurando o Banco de Dados

--- index.ts
+++ index.ts
@@ -1,4 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
+import { routes } from './src/routes';
+
+import './src/database';
 
 const app = express();
-app.listen(3333, () => console.log('Funcionou'));
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => console.log('Valoriza is Online'));
